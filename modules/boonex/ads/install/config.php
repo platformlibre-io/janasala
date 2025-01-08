56553713074034584514ebd508acf70b9b28bea9
Chinthaka Deshapriya <33759310+cdeshapriya@users.noreply.github.com>
2025-01-08 06:19:40 +0530

    Update config.php

--- modules/boonex/ads/install/config.php
+++ modules/boonex/ads/install/config.php
@@ -18,8 +18,8 @@ $aConfig = array(
     'name' => 'bx_ads',
     'title' => 'Ads',
     'note' => 'Ads module.',
-    'version' => '14.0.5.DEV',
-    'vendor' => 'UNA INC',
+    'version' => '14.0.5.RC1',
+    'vendor' => 'Libresoft Tech',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
